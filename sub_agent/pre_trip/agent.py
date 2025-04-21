72288344f51b31f6ff9973e1ab56976f3813ecdb
Vinit <141900819+vinit-creo@users.noreply.github.com>
2025-04-21 14:19:26 +0530

    - add basic propmts for pre_trip stage

---
+++ sub_agent/pre_trip/agent.py
@@ -0,0 +1,52 @@
+from google.adk.agents import Agent
+
+from sub_agent.pre_trip import prompt
+
+
+
+place_agent = Agent(
+    model="gemini-2.0-flash",
+    name="place_agent",
+    instruction=prompt.PLACE_AGENT_INSTR,
+    description="This agent suggests a few destination given some user preferences",
+    disallow_transfer_to_parent=True,
+    disallow_transfer_to_peers=True,
+    output_schema=DesintationIdeas,
+    output_key="place",
+    generate_content_config=json_response_config,
+)
+
+
+poi_agent = Agent(
+    model="gemini-2.0-flash",
+    name="poi_agent",
+    description="This agent suggests a few activities and points of interests given a destination",
+    instruction=prompt.POI_AGENT_INSTR,
+    disallow_transfer_to_parent=True,
+    disallow_transfer_to_peers=True,
+    output_schema=POISuggestions,
+    output_key="poi",
+    generate_content_config=json_response_config,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pre_trip_agent = Agent(
+        model="gemini-2.0-flash",
+    name="pre_trip_agent",
+    description="A travel inspiration agent who inspire users, and discover their next vacations; Provide information about places, activities, interests,",
+    instruction=prompt.PRE_TRIP_AGENT_INSTR,
+    tools=[AgentTool(agent=place_agent), AgentTool(agent=poi_agent), map_tool],
+
+)
